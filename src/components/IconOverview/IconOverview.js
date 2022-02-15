458a935b8130fdd8b7d98f25f359693ae42a8945
Alyssa Durante <alyssa.durante@answerrocket.com>
2022-02-15 17:45:47 -0500

    updating

--- src/components/IconOverview/IconOverview.js
+++ src/components/IconOverview/IconOverview.js
@@ -7,7 +7,6 @@ import './icon-overview.scss';
 class IconOverview extends React.Component {
   renderItems = (currentItem) => {
     const { icon } = currentItem;
-    console.log(currentItem.svgLink)
     let iconUrl;
     iconUrl = `https://app.streamlinehq.com/icons/streamline-bold/${currentItem.svgLink
         .toLowerCase()
@@ -20,9 +19,12 @@ class IconOverview extends React.Component {
       // eslint-disable-next-line global-require
       iconImg = require('./images/NoImage.svg').default;
     }
-
+    Object.entries(iconList).map((icons) => {
+     console.log(icons)
+  })
+    
     return (
-      <li className="component-item" key={icon}>
+      <li className="component-item m-10" key={icon}>
         <div className="bx--aspect-ratio bx--aspect-ratio--align bx--aspect-ratio--1x1">
           <div className="bx--aspect-ratio--object">
           <Link to={iconUrl} className="component-item__link" target="_blank">
@@ -33,6 +35,7 @@ class IconOverview extends React.Component {
               />
               <p className="component-name">{icon}</p>
               </Link>
+              <h5>When to Use</h5>
           </div>
         </div>
       </li>
@@ -41,6 +44,7 @@ class IconOverview extends React.Component {
 
   render() {
     return (
+      <div>
       <div className="bx--row">
         <div className="bx--col-lg-12 bx--no-gutter">
           <ul className="component-overview">
@@ -49,6 +53,7 @@ class IconOverview extends React.Component {
             )}
           </ul>
         </div>
+      </div>
       </div>
     );
   }
